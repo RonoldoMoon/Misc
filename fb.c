f894a9aa853bb7b56a221a984ff996f140fc1931
RonoldoMoon <40582496+RonoldoMoon@users.noreply.github.com>
2019-04-14 04:49:18 +0000

    Add files via upload

--- fb.c
+++ fb.c
@@ -16,7 +16,7 @@ int main()  {
   int fd, x = 0, y = 0;
   struct fb_var_screeninfo vinfo;
   struct fb_fix_screeninfo finfo;
-  uint8_t   *p;
+  uint32_t   *p; //correct pointer size.
   uint32_t   screensize;
   if ((fd = open("/dev/fb0", O_RDWR)) == -1)  {
   
@@ -44,7 +44,16 @@ int main()  {
   screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
   
   printf("buffer size: %d bytes\n", screensize);
+  /*
+  puts("attempting to set");
+  vinfo.xres = 640;
+  vinfo.yres = 480;
   
+  if (ioctl(fd, FBIOPUT_VSCREENINFO, &vinfo) == -1) { //put
+    
+    puts("shit SETTING");
+    exit(1);
+  }*/
   if ((p = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) == -1 )  {
 	  
     perror("mmap(): ");
@@ -53,7 +62,8 @@ int main()  {
   
   for ( x =  0; x < 0xffff; x++ ) {
 	  
-    p[x] = 0x30;
+    p[x] = 0x0000ff00;
+    //       ??RRGGBB
   }
   
   munmap(p, screensize);
